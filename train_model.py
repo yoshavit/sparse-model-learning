aa7948548eb8f2cb308e7dacd921334fda077d91
Yonadav Shavit <yonadav@mit.edu>
2017-06-27 11:49:11 -0400

    About to do major surgery on the supervisor workflow

--- train_model.py
+++ train_model.py
@@ -37,7 +37,7 @@ parser.add_argument('--ignore-latent-consistency', help="Penalize latent encodin
 parser.add_argument('-batchsize', help="Batch size for learning", type=int,
                     default=16)
 parser.add_argument('--max-steps', type=int, default=10000000, help="Number of steps to train (if 0, trains until manually halted)")
-parser.add_argument('-stepsize', type=float, default=1e-3, help="train step size")
+parser.add_argument('-stepsize', type=float, default=1e-4, help="train step size")
 parser.add_argument('--latent-size', type=int, default=32, help="Number of latent dimensions used to encode the state")
 
 args = parser.parse_args()
@@ -46,14 +46,14 @@ max_horizon = 1
 # ------ Define features ------------
 # Feature will be true MNIST digit
 # Feature is extracted from info
-feature_extractor = lambda info: [info==0]
+feature_extractor = lambda info: [info]
 feature_size = 1
 # Label is separate from info
 label_extractor = lambda info: [info]
 # ----------------------------------
 logdir = os.path.join('data', args.env, args.log_dir, 'train')
 if args.run_id:
-    logdir += args.run_id
+    logdir = os.path.join(logdir, args.run_id)
 else: # increment until we get a new id
     logdir = increment_path(os.path.join(logdir, "run"))
 logger.info("Logging results to {}".format(logdir))
@@ -63,31 +63,33 @@ with tf.variable_scope("model"):
                       latent_size=args.latent_size,
                       max_horizon=max_horizon,
                       summary_writer=sw,
-                      force_latent_consistency=not args.ignore_latent_consistency,
+                      force_latent_consistency=(not
+                                                args.ignore_latent_consistency),
                       feature_extractor=feature_extractor)
 saver = tf.train.Saver()
+savepath = os.path.join(logdir, "model.ckpt")
 logger.info("Gathering initial gameplay data!")
 ml.gather_gameplay_data(500)
 init_op = tf.global_variables_initializer()
 init_fn = lambda sess: sess.run(init_op)
 sv = tf.train.Supervisor(logdir=logdir,
-                         saver=saver,
+                         saver=None,
                          init_op=init_op,
                          init_fn=init_fn,
                          summary_op=None,
                          summary_writer=sw,
                          global_step=ml.global_step,
-                         save_model_secs=30,
-                         save_summaries_secs=15)
-with sv.managed_session() as sess:
+                         save_model_secs=0,
+                         save_summaries_secs=20)
+with tf.Session() as sess:
     global_step = sess.run(ml.global_step)
-    sw.add_graph(sess.graph)
     logger.info("Beginning training.")
     logger.info("To visualize, call:\ntensorboard --logdir={}".format(logdir))
     while not sv.should_stop() and ((not args.max_steps) or global_step <
                                     args.max_steps):
         transition_data = ml.create_transition_dataset(max_horizon,
                                                        label_extractor=label_extractor)
+        print(savepath, sv.save_path)
         for batch in dataset.iterbatches(transition_data,
                                          batch_size=args.batchsize,
                                          shuffle=True):
@@ -99,6 +101,9 @@ with sv.managed_session() as sess:
                                              batch_size=ml.num_embed_vectors,
                                              shuffle=False))
             ml.train_model(sess, *batch, show_embeddings=True)
+            # By only saving during the embedding phase, our embedding isn't
+            # overwritten by other saves
+            saver.save(sess, savepath)
         global_step = sess.run(ml.global_step)
         ml.gather_gameplay_data(10)
 logger.info("Training complete!")
